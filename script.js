4af5ed6ee1dd3efb183842565bc7f7b12d10c053
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2024-03-05 10:19:52 +0530

    Done

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-const Url = `https://emoji-api.com/emojis?access_key=4e1774d191b487990ca6e78692478dc7e6b7ecd8`
+const Url = `https://emoji-api.com/emojis?access_key=4e1774d191b487990ca6e78692478dc7e6b7ecd8`;
 let Emojis = [];
-let allEmojiContainer = document.querySelector(".allEmoji")
-async function allEmoji(){
-let response = await fetch(Url)
-let data = await response.json()
-for (let i = 0; i < data.length; i++) {
-    Emojis.push(data[i].character)
+let allEmojiContainer = document.querySelector(".allEmoji");
+
+async function allEmoji() {
+    let response = await fetch(Url);
+    let data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+        Emojis.push(data[i].character);
+    }
 }
-}
 
-allEmoji()
+allEmoji();
 
+let btn = document.getElementById("btn");
 
-let btn = document.getElementById("btn")
-btn.addEventListener("mouseover", ()=>{
-    btn.innerText = Emojis[Math.floor(Math.random() * 1859)]
-    
-})
-btn.addEventListener("click", ()=>{
-    btn.innerText = Emojis[Math.floor(Math.random() * 1859)]
+btn.addEventListener("mouseover", () => {
+    if (Emojis.length > 0) {
+        btn.innerText = Emojis[Math.floor(Math.random() * Emojis.length)];
+    } else {
+        btn.innerText = "😊";
 
-})
+    }
+});
+
+btn.addEventListener("click", () => {
+    if (Emojis.length > 0) {
+        btn.innerText = Emojis[Math.floor(Math.random() * Emojis.length)];
+    } else {
+        btn.innerText = "😊";
+
+    }
+});
+
